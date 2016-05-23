29ed7c1912de92bc2b7fa857ff6b321df70c676b
mikermcnally <mikemcnally@gmail.com>
2016-05-23 08:03:00 -0400

    fixed findobjs

--- Roll20.js
+++ Roll20.js
@@ -1,3 +1,39 @@
+var generateUUID = (function() {
+    "use strict";
+
+    var a = 0,
+        b = [];
+    return function() {
+        var c = (new Date()).getTime() + 0,
+            d = c === a;
+        a = c;
+        for (var e = new Array(8), f = 7; 0 <= f; f--) {
+            e[f] = "-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz".charAt(c % 64);
+            c = Math.floor(c / 64);
+        }
+        c = e.join("");
+        if (d) {
+            for (f = 11; 0 <= f && 63 === b[f]; f--) {
+                b[f] = 0;
+            }
+            b[f]++;
+        } else {
+            for (f = 0; 12 > f; f++) {
+                b[f] = Math.floor(64 * Math.random());
+            }
+        }
+        for (f = 0; 12 > f; f++) {
+            c += "-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz".charAt(b[f]);
+        }
+        return c;
+    };
+}()),
+
+generateRowID = function() {
+    "use strict";
+    return generateUUID().replace(/_/g, "Z");
+};
+
 var Roll20 = Roll20 || (function(context){
     context.state = {};
 
@@ -22,8 +58,8 @@ var Roll20 = Roll20 || (function(context){
     }
     
     context.findObj = function (opts){
-        _.each(Roll20.objstore, function (obj){
-             
+        return _.filter(Roll20.objstore, function(obj){
+            return _.isMatch(obj._attributes, opts);
         });
     }
    
@@ -43,19 +79,20 @@ var R20 = Roll20;
 
 Roll20.objstore = {};
 
-
 Roll20.Object = function (type, attributes){
+    var id = generateRowID(); 
     attributes = attributes || {};
     attributes._type = type;
-    attributes._id = this._id;    
+    attributes._id = id;
     this._type = type;
     this._attributes = attributes;
+    this.id = id;
     
-    Roll20.objstore[this._type] = Roll20.objstore[this._type] || {};
-    Roll20.objstore[this._type][this._id] = this;
+    Roll20.objstore[this.id] = Roll20.objstore[this.id] || {};
+    Roll20.objstore[this.id] = this;
 }
 Roll20.Object.index={};
-Roll20.Object.prototype._id = (function (){return this._id || _.uniqueId()})();
+Roll20.Object.prototype._id = (function (){return this._id || generateRowID()})();
 Roll20.Object.prototype.get = function (attr){
      return this._attributes[attr];
 }
@@ -64,3 +101,4 @@ Roll20.Object.prototype.set = function (attr, value){
 }
 
 Roll20.objstore.campaign = new Roll20.Object;
+
